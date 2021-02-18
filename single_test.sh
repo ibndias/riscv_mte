9f295cd5709ddc8730a114f6108bcf7ff1d47181
Derry Pratama <derry@islab.re.kr>
2021-02-18 14:32:55 +0900

    Modify single_test.sh to run qemu & gdb

--- single_test.sh
+++ single_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #set -x
 echo "Running QEMU..."
-./scripts/run_qemu.sh ${1} . ; echo $?
+./scripts/run_qemu.sh ${1} . &
+riscv64-unknown-elf-gdb -ex 'target remote :1234' -ex 'b main' -ex 'layout regs' -ex 'c' ${1}
+echo $?
