bd22737266bdfc8e3304f29736c12424172a7ff0
Derry Pratama <derry@islab.re.kr>
2021-03-10 17:12:39 +0900

    Test read write access

--- single_test.sh
+++ single_test.sh
@@ -2,7 +2,7 @@
 #set -x
 echo "Running QEMU..."
 ./scripts/run_qemu_dbg.sh ${1} . &
-#gnome-terminal -e "./run_gdb.sh ${1}" --window --maximize
+gnome-terminal -e "./run_gdb.sh ${1}" --window --maximize
 
 
 
