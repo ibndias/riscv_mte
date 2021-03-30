e0c0fb3739f3367b4102785e19e358b958b5f229
Derry Pratama <derry@islab.re.kr>
2021-03-30 09:54:35 +0900

    use own qemu

--- scripts/run_qemu_dbg.sh
+++ scripts/run_qemu_dbg.sh
@@ -6,11 +6,13 @@ set -e
 source ${2}/scripts/env.sh
 
 QEMU_BIN=${INSTALL_DIR}/bin/qemu-system-riscv64
+#QEMU_BIN=qemu-system-riscv64
 
 ${QEMU_BIN} \
     -nographic \
     -bios none -s -S\
     -machine virt -singlestep \
+    #-plugin ../qemu/build/tests/plugin/libhowvec.so,arg=a0 -d plugin -D debug_out.txt\
     -kernel ${1} \
     -m 1024M
 exit $?
